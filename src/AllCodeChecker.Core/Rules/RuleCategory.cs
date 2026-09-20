c95ba107bcc1da4748f3bb8cdc174b548c9653fb
kadoka <tomiya3499@gmail.com>
2026-09-20 15:30:51 +0900

    Add ACI rule categories

---
+++ src/AllCodeChecker.Core/Rules/RuleCategory.cs
@@ -0,0 +1,15 @@
+namespace AllCodeChecker.Rules;
+
+public enum RuleCategory
+{
+    FundamentalCorrectness,
+    RuntimeBehavior,
+    Maintainability,
+    TestAndCoverage,
+    ConfigurationAndProjectMetadata,
+    Security,
+    DataBoundary,
+    DependencyAndEnvironment,
+    LanguageSpecific,
+    Internal
+}
